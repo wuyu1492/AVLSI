cb9503f6787d5729cd2e0e9b6d52c5cfef58582d
WU YU <b03901115@ntu.edu.tw>
2017-12-26 10:57:35 +0800

    Add files via upload

---
+++ test-surgeon.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import numpy as np
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import BatchNormalization
+from keras.optimizers import Adadelta
+from kerassurgeon.operations import delete_channels
+import sys
+# plot
+
+
+def read_train(fn):
+    df = pd.read_csv(fn, sep=" |,",header=None,
+            engine='python',
+            skiprows=[0])
+    data = df.values
+    label_int = data[:,0]
+    label = to_categorical(label_int, 7)
+    feature = data[:,1:].astype(np.float)
+    feature -= np.mean(feature, axis=1).reshape(-1,1)
+    #feature /= (np.std(feature, axis=1).reshape(-1,1) + 1e-8)
+    return label, feature
+
+def show(img, label):
+    plt.figure
+    plt.imshow(img, cmap='gray')
+    label_str = "image with label : " + str(np.argmax(label))
+    plt.title(label_str)
+    plt.show()
+    plt.savefig('show_pic.png')
+
+def train_cnn_model(label, feature):
+    feature = np.reshape(feature, (-1, 48, 48, 1))
+    split = 0.8
+    split *= len(label)
+    split = np.round(split).astype(np.int)
+    label_train = label[:split]
+    x_train = feature[:split]
+    label_val = label[split:]
+    x_val = feature[split:]
+
+    # cnn model
+    model = Sequential()
+    model.add(Conv2D(32,(5,5),padding='same',input_shape=(48,48,1)))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=5,strides=2))
+    model.add(Dropout(0.4))
+
+    model.add(Conv2D(64,(3,3),padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=3,strides=2))
+    model.add(Dropout(0.4))
+    
+    model.add(Conv2D(128,(3,3),padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=3,strides=2))
+    model.add(Dropout(0.4))
+
+    model.add(Flatten())
+    model.add(Dense(1024))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(Dropout(0.4))
+    model.add(Dense(1024))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(Dropout(0.4))
+    model.add(Dense(7))
+    model.add(Activation("softmax"))
+    
+    model.compile(loss='categorical_crossentropy',
+            optimizer='adam',
+            metrics=['accuracy'])
+    model.summary()
+
+    # prune the model
+    model = delete_channels(model, model.layers[9], [0, 4 , 8, 16, 32, 64])
+    model.compile(loss='categorical_crossentropy',
+            optimizer='adam',
+            metrics=['accuracy'])
+    model.summary()
+    
+    # train model
+    batch = 128
+    epo = 30
+
+    history = model.fit(x_train,
+            label_train, 
+            batch_size=batch,
+            epochs=epo,
+            validation_data=(x_val, label_val))
+    model.save("model_cnn.h5")
+    np.save("history_acc.npy", history.history['acc'])
+    np.save("history_val_acc.npy", history.history['val_acc'])
+    np.save("cnn_x_val.npy", x_val)
+    np.save("cnn_label_val.npy", label_val)
+    
+    #plot_saliency(model, x_val[0], str(np.argmax(label_val[0])))
+
+def main(argv):
+    label, feature = read_train(argv[0])
+    train_cnn_model(label, feature)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
